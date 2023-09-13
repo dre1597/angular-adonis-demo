5f22b4dc8b6214e362d3e30043d048eac4a35058
André Pereira <andrepereira97@yahoo.com>
2023-09-13 19:02:54 -0300

    :truck: organize folders

--- mangalist-front/src/app/shared/shared.module.ts
+++ mangalist-front/src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardModule } from 'primeng/card';
 
-import { FormCardComponent } from './form-card/form-card.component';
+import { FormCardComponent } from './form/card/form-card.component';
 
 const primeNgModules = [CardModule];
 
