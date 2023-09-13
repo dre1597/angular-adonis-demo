370414d8e321dde848c43f77872b6e9e6114a9d9
André Pereira <andrepereira97@yahoo.com>
2023-09-13 19:05:39 -0300

    :recycle: make template and styles inline

--- mangalist-front/src/app/shared/form/card/form-card.component.ts
+++ mangalist-front/src/app/shared/form/card/form-card.component.ts
@@ -1,9 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-form-card',
-  templateUrl: './form-card.component.html',
-  styleUrls: ['./form-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <p-card [style]="{ width: '30%', margin: '50px auto' }">
+      <p-header class="header">
+        <h1 class="header">{{ title }}</h1>
+      </p-header>
+      <ng-content select="[body]"></ng-content>
+      <p-footer>
+        <div class="flex justify-between">
+          <ng-content select="[footer]"></ng-content>
+        </div>
+      </p-footer>
+    </p-card>
+  `,
+  styles: [
+    `
+      .header {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 0;
+      }
+    `,
+  ],
 })
 export class FormCardComponent {
   @Input({ required: true }) title!: string;
